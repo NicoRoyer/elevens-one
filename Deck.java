a5d80a349673a868a016b8d952c45b092f99912c
nicoroyer <nicholas.royer@gmail.com>
2020-03-12 10:28:50 -0500

    death is an escape

--- Deck.java
+++ Deck.java
@@ -30,7 +30,12 @@ public class Deck {
 	 * @param values is an array containing all of the card point values.
 	 */
 	public Deck(String[] ranks, String[] suits, int[] values) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		for(int j = 0;j < suits.length;j++){
+			for(int i = 0; i < ranks.length;i++){
+				this.cards.add(new Card(ranks[i],suits[j],values[i]));
+			}
+		}
+		size = cards.size();
 	}
 
 
@@ -39,7 +44,12 @@ public class Deck {
 	 * @return true if this deck is empty, false otherwise.
 	 */
 	public boolean isEmpty() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		if(this.cards.size() == 0){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 
 	/**
@@ -47,7 +57,7 @@ public class Deck {
 	 * @return the number of undealt cards in this deck.
 	 */
 	public int size() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		return this.cards.size();
 	}
 
 	/**
@@ -64,7 +74,9 @@ public class Deck {
 	 *         previously dealt.
 	 */
 	public Card deal() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		Card z = cards.get(0);
+		cards.remove(0);
+		return z;
 	}
 
 	/**
--- DeckTester.java
+++ DeckTester.java
@@ -1,3 +1,5 @@
+import java.util.List;
+import java.util.ArrayList;
 /**
  * This is a class that tests the Deck class.
  */
@@ -8,6 +10,12 @@ public class DeckTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		String[] ranks = {"Ace","King"};
+		String[] suits = {"black","red"};
+		int[] value = {1,13};
+
+		Deck rigged = new Deck(ranks,suits,value);
+		System.out.println(rigged.isEmpty());
+		System.out.println(rigged.deal());
 	}
 }
